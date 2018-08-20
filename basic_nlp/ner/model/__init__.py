48f98cd17df680c6da46e7094f63ca37c8723dc5
fay111101 <1139598278@qq.com>
2018-08-20 18:43:36 +0800

    ner 参考博客

--- basic_nlp/ner/model/__init__.py
+++ basic_nlp/ner/model/__init__.py
@@ -12,7 +12,9 @@ https://blog.csdn.net/sinat_33741547/article/details/79131223
 https://github.com/Determined22/zh-NER-TF
 
 重要！！！
+NER
 https://blog.csdn.net/guolindonggld/article/details/79044574
 https://www.lookfor404.com/%E7%94%A8%E5%8F%8C%E5%90%91lstmcrf%E5%81%9A%E5%91%BD%E5%90%8D%E5%AE%9E%E4%BD%93%E8%AF%86%E5%88%AB%E9%99%84tensorflow%E4%BB%A3%E7%A0%81-ner%E7%B3%BB%E5%88%97%EF%BC%88%E5%9B%9B/
 https://www.cnblogs.com/Dream-Fish/p/3956901.html
+https://www.cnblogs.com/Determined22/p/7238342.html
 """
